543e1aedc5b35ff0818cc7ebb4b6307ba344ec26
Fabrice Andreis <fabrice.andreis@hotmail.com>
2015-10-01 13:50:02 +0200

    Demo Data and Model configuration

--- source/DotNetBay.WPF/App.xaml.cs
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DotNetBay.Core;
 using DotNetBay.Core.Execution;
 using DotNetBay.Data.FileStorage;
 using DotNetBay.Interfaces;
+using DotNetBay.Model;
 
 namespace DotNetBay.WPF
 {
@@ -24,6 +26,22 @@ namespace DotNetBay.WPF
         {
             auctionRunner = new AuctionRunner(this.MainRepository);
             AuctionRunner.Start();
+
+
+            var memberService = new SimpleMemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository, memberService);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
         }
     }
 }
