4b5380cf62306c68f34a967467d1c7924c815c0a
Robert Rajakone <robie2011@gmail.com>
2015-10-16 20:46:26 +0200

    adding openfile dialog

--- source/DotNetBay.WPF/NewAuctionView.xaml.cs
+++ source/DotNetBay.WPF/NewAuctionView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,15 @@ namespace DotNetBay.WPF
         public NewAuctionView()
         {
             InitializeComponent();
+        }
+
+        private void ShowFileDialog(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                txtImagePath.Text = dialog.FileName;
+            }
         }
     }
 }
