4a7fa08b5d149b183eefbff656d793c37b210d29
Robert Rajakone <robie2011@gmail.com>
2015-10-17 13:49:42 +0200

    Lab03 Reordering files

---
+++ source/DotNetBay.WPF/Features/Overview/BooleanToStateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DotNetBay.WPF
+{
+
+    public class BooleanToStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //should always work. Otherwise we have corrupt data and the application shall crash
+            return (bool)value ? "Offen" : "Abgeschlossen";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- source/DotNetBay.WPF/Features/Overview/MainWindow.xaml.cs
+++ source/DotNetBay.WPF/Features/Overview/MainWindow.xaml.cs
@@ -56,19 +56,4 @@ namespace DotNetBay.WPF
             newAuctionview.ShowDialog();
         }
     }
-
-
-    public class BooleanToStateConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            //should always work. Otherwise we have corrupt data and the application shall crash
-            return (bool)value ? "Offen" : "Abgeschlossen";            
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
