80a84dd9168dc26c541b237fc06155356b5b9a70
Robert Rajakone <robie2011@gmail.com>
2015-10-19 16:21:41 +0200

    Lab04 Init DotNetBay.Data.EF Project

---
+++ source/DotNetBay.Data.EF/MainDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBay.Data.EF
+{
+    public class MainDbContext : System.Data.Entity.DbContext
+    {
+
+    }
+}
