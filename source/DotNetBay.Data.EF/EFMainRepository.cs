b5782e57d6895ccabf388b22dc3e0b9850fb13cd
Robert Rajakone <robie2011@gmail.com>
2015-10-19 17:55:58 +0200

    Lab04 Adding app.config, EF Repository and Factory

---
+++ source/DotNetBay.Data.EF/EFMainRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBay.Interfaces;
+using DotNetBay.Model;
+
+namespace DotNetBay.Data.EF
+{
+    public class EFMainRepository : IMainRepository
+    {
+        public IQueryable<Auction> GetAuctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Member> GetMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Auction Add(Auction auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Auction Update(Auction auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bid Add(Bid bid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bid GetBidByTransactionId(Guid transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Member Add(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/DotNetBay.Data.EF/EFMainRepositoryFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.Data.EF
+{
+    public class EFMainRepositoryFactory : IRepositoryFactory
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMainRepository CreateMainRepository()
+        {
+            return new EFMainRepository();
+        }
+    }
+}
